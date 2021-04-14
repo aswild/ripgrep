eed45cdf7b97a53701f5aae24b4b317a17c60f50
Allen Wild <allenwild93@gmail.com>
2021-04-14 10:52:28 -0400

    ci: add scripts to build a deb for Debian Buster
    glibc is fine, but build it in a container to avoid contamination of
    incompatible libs in Arch.

---
+++ ci/docker/buster/build-deb.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+run() {
+    echo "+ $*" >&2
+    "$@"
+}
+
+set -e
+
+if [[ ! -f Cargo.lock ]]; then
+    echo "Please run from the root of ripgrep's repo" >&2
+    exit 1
+fi
+
+DOCKER_IMAGE=aswild/ripgrep-builder:buster
+_uid=$(id -u)
+_gid=$(id -g)
+
+if ! docker inspect --type image $DOCKER_IMAGE &>/dev/null; then
+    echo >&2 "Image $DOCKER_IMAGE not found, building"
+    run docker build -t $DOCKER_IMAGE ci/docker/buster
+fi
+
+run docker run -t --rm \
+    --user "${_uid}:${_gid}" \
+    --volume "$PWD:/ripgrep" \
+    --workdir /ripgrep \
+    $DOCKER_IMAGE \
+    ci/build-deb
