06b1ef5aeea4b7515b84fe8e0615717eaf9dbf47
Allen Wild <allenwild93@gmail.com>
2023-08-05 23:45:08 -0400

    ci: update bullseye container to bookworm

--- ci/docker/bookworm/build-deb.sh
+++ ci/docker/bookworm/build-deb.sh
@@ -12,13 +12,13 @@ if [[ ! -f Cargo.lock ]]; then
     exit 1
 fi
 
-DOCKER_IMAGE=aswild/ripgrep-builder:bullseye
+DOCKER_IMAGE=aswild/ripgrep-builder:bookworm
 _uid=$(id -u)
 _gid=$(id -g)
 
 if ! docker inspect --type image $DOCKER_IMAGE &>/dev/null; then
     echo >&2 "Image $DOCKER_IMAGE not found, building"
-    run docker build -t $DOCKER_IMAGE ci/docker/bullseye
+    run docker build -t $DOCKER_IMAGE ci/docker/bookworm
 fi
 
 run docker run -ti --rm \
